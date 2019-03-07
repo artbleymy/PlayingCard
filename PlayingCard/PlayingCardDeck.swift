e6974b23a1420258779a4689ac58cc64119c0060
Станислав Козлов <bleymy@mail.ru>
2019-03-07 15:14:36 +0300

    Add images and drawing cards

---
+++ PlayingCard/PlayingCardDeck.swift
@@ -0,0 +1,44 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by Stanislav on 06.03.2019.
+//  Copyright © 2019 Stanislav Kozlov. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all{
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+//MARK:  - extend Int class for getting random Int value
+extension Int{
+    var arc4random: Int {
+        if self > 0{
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+        
+    }
+}
